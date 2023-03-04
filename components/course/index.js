63d4eddb6634bb63834ba462b5ae24ac30034836
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-03-04 14:19:31 +0000

    Project initiation

---
+++ components/course/index.js
@@ -0,0 +1,6 @@
+
+
+export { default as List } from './list'
+export { default as Hero } from './hero'
+export { default as Keypoints } from './keypoints' 
+export { default as Cirriculum} from './cirriculum'
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import '@styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const Layout = Component.Layout
+
+  return (
+    <Layout>
+        <Component {...pageProps} />
+    </Layout>
+  )
 }
