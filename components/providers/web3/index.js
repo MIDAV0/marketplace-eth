69d3f7e37da4e48efca5db6ec3c32c9333c3b469
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-03-06 02:06:20 +0000

    Refactored folders and created web3 provider

---
+++ components/providers/web3/index.js
@@ -0,0 +1,60 @@
+const { createContext, useContext } = require('react');
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+import { useState, useEffect } from 'react';
+
+
+const Web3Context = createContext(null);
+
+export default function Web3Provider({ children }){
+    const [web3Api, setWeb3Api] = useState(
+        {
+            provider: null,
+            web3: null,
+            contract: null,
+            isLoading: true,
+        }
+    )
+
+    useEffect(() => {
+        const loadProvider = async () => {
+            const provider = await detectEthereumProvider()
+            if (provider) {
+                const web3 = new Web3(provider)
+                setWeb3Api({
+                    provider,
+                    web3,
+                    contract: null,
+                    isLoading: false,
+                })
+            } else {
+                setWeb3Api(api => ({
+                    ...api,
+                    isLoading: false,
+                }))
+                console.error('Please install MetaMask!')
+            }
+        }
+
+        loadProvider()
+    }, [])
+
+    const _web3Api = useMemo(() => {
+        return {
+            ...web3Api,
+            connect: () => console.log("Trying to connect!"),
+            test: () => console.log("Hello World!")
+        }
+    }, [web3Api])
+
+
+    return (
+        <Web3Context.Provider value={web3Api}>
+            {children}
+        </Web3Context.Provider>
+    )
+}
+
+export function useWeb3(){
+    return useContext(Web3Context)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 
-import { Hero } from "@components/common"
-import { BaseLayout } from "@components/layout"
-import { Card } from "@components/order"
+import { Hero } from "@components/ui/common"
+import { BaseLayout } from "@components/ui/layout"
+import { Card } from "@components/ui/order"
 import { getAllCourses } from "@content/courses/fetcher"
 
 export default function Home({ courses }) {
+  
   return (
         <>
           <Hero />
