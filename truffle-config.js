e903d9dfd98efa5be5adc9888650343331df44bf
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-05-01 15:01:50 +0100

    Network change to Goerli

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
 
 module.exports = {
   contracts_build_directory: "./public/contracts",
@@ -7,16 +9,18 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    //
-    // An additional network, but with some advanced options…
-    // advanced: {
-    //   port: 8777,             // Custom port
-    //   network_id: 1342,       // Custom network
-    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    //   from: <address>,        // Account to send transactions from (default: accounts[0])
-    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-    // },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          keys.MNEMONIC,
+          keys.INFURA_PROJECT_ID
+        ),
+      network_id: 5,
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2, 
+      timeoutBlocks: 200,
+    }
   },
 
   // Configure your compilers
