eec6dc99ab33665fce6cc8a3fb92c1c99505965a
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-05-02 17:51:34 +0100

    Repurchase

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -147,7 +147,10 @@ export default function Marketplace({ courses }) {
                             variant="white"
                             disabled={true}
                             size="sm">
-                            Loading State...
+                              { hasConnectedWallet ?
+                                "Loading..." :
+                                "Connect"  
+                              }
                           </Button>
                         )
                       }
